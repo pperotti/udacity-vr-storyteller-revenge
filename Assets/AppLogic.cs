518410c3788db04957f24047d6e55c2049378660
Pablo Perotti <pablo@blackpixel.com>
2017-07-24 02:45:46 -0300

    chore: Update player controls.

---
+++ Assets/AppLogic.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppLogic : MonoBehaviour
+{
+	public void LoadScene(string sceneName) 
+	{
+		Application.LoadLevel(sceneName);
+	}
+
+}
